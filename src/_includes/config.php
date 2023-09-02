6fa64e64c33fe405b8904ec9a37e860baf2ce7e5
PaulaWhitefield <paula.whitefield@themailbox.io>
2023-09-02 16:37:09 +0100

    Updated Staticly WebFonts URL

--- src/_includes/config.php
+++ src/_includes/config.php
@@ -6,6 +6,6 @@ $site_author = "Paula Whitefield";
 $site_author_url = "/";
 
 $images_url = "https://images.storage.ict.rocks";
-$fonts_url = "https://webfonts.staticly.cc";
+$fonts_url = "https://staticly.cc/webfonts";
 
 $blog_datetime_format = "M j, Y \a\\t H:i";
