a98404c7478c3082b7a3f5f9564b15b514b83358
PaulaWhitefield <paula.whitefield@themailbox.io>
2023-09-13 22:27:47 +0100

    Changed webfonts URL

--- src/_includes/config.php
+++ src/_includes/config.php
@@ -6,6 +6,6 @@ $site_author = "Paula Whitefield";
 $site_author_url = "/";
 
 $images_url = "https://images.storage.ict.rocks";
-$fonts_url = "https://staticly.cc/webfonts";
+$fonts_url = "https://webfonts.storage.ict.rocks";
 
 $blog_datetime_format = "M j, Y \a\\t H:i";
