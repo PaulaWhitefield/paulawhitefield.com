ff172a31aa2621971576fd6a43eaa0b397dd668d
PaulaWhitefield <paula.whitefield@themailbox.io>
2025-01-02 15:59:02 +0000

    Added new blog post about new antiland link.

---
+++ src/blog/2025/01/02/new-antiland-link.php
@@ -0,0 +1,20 @@
+---
+article_title: New AntiLand Link
+article_description: Future-proofing against future AntiLand link changes.
+article_author: Paula Whitefield
+article_date: 2025-01-02
+article_time: 16:00
+
+current_nav_item: blog
+layout: src/_includes/base.php
+---
+
+<p>Luckily, I have never had an issue accessing or using AntiLand and chatting to those who wish to chat with me directly on that platform, however, my AntiLand link could change at any time by the AntiLand team, and I don't like that possibility when there is something I can do about it.</p>
+
+<p>My friend owns and operates the WebURL.net domain for the purpose of creating permanent URLs, or "permalinks", to various things online, and has given me free rein over my own permalinks, so I have decided to create one to find me on AntiLand. This gives me full control over this link, removing the worry that the AntiLand team could change it because they simply cannot change this link, only me and my friend can.</p>
+
+<p>The new permalink to find me on AntiLand is: <a href="https://weburl.net/paula/antiland" target="_blank">https://weburl.net/paula/antiland</a></p>
+
+<p>Please update any favorites/bookmarks where you may have the old link, as this link will not change and will always land you in the right place.</p>
+
+<p>Happy chatting!</p>
